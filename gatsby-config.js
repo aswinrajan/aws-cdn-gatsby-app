8138ca33827e4a4e3c0f16da117b33ff3e190a7e
Aswin Rajan <aswin.soundararajan@gmail.com>
2021-12-16 06:38:37 +0000

    updated workflows

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Aswin`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Aswin Soundararajan`,
+    siteTitleAlt: `Aswin Soundararajan`,
+    siteHeadline: `Aswin`,
+    siteUrl: `https://aswinrajan.ca/`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `aswinsoundararajan`,
   },
   plugins: [
     {
